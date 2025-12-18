0964f965f126ab2f254cbfb93e64335284e58d32
thiagosullivan <thiagosullivan12@gmail.com>
2025-12-18 07:52:39 -0300

    chore: add routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import { Route, Routes, useLocation } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Movies from "./pages/Movies";
+import SeatLayout from "./pages/SeatLayout";
+import MovieDetails from "./pages/MovieDetails";
+import MyBookings from "./pages/MyBookings";
+import Favorite from "./pages/Favorite";
 
 const App = () => {
-  return <div>App</div>;
+  const isAdminRoute = useLocation().pathname.startsWith("/admin");
+
+  return (
+    <>
+      {!isAdminRoute && <Navbar />}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/movies/:id" element={<MovieDetails />} />
+        <Route path="/movies/:id/:date" element={<SeatLayout />} />
+        <Route path="/my-bookings" element={<MyBookings />} />
+        <Route path="/favorite" element={<Favorite />} />
+      </Routes>
+    </>
+  );
 };
 
 export default App;
