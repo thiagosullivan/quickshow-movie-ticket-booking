46f691a79144ecd4c0478043c4d05d527c77fe60
thiagosullivan <thiagosullivan12@gmail.com>
2025-12-19 11:07:30 -0300

    feat: hero section

---
+++ client/src/components/HeroSection.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { assets } from "../assets/assets";
+import { ArrowRight, CalendarIcon, ClockIcon } from "lucide-react";
+import { useNavigate } from "react-router-dom";
+
+const HeroSection = () => {
+  const navigate = useNavigate();
+
+  return (
+    <section className='flex flex-col items-start justify-center gap-4 px-6 md:px-16 lg:px-36 bg-[url("/backgroundImage.png")] bg-cover bg-center h-screen'>
+      <img src={assets.marvelLogo} alt="Hero image" />
+
+      <h1 className="text-5xl md:text-[70px] md:leading-18 font-semibold">
+        Guardians <br /> of the Galaxy
+      </h1>
+
+      <div className="flex items-center gap-4 text-gray-300">
+        <span>Action | Adventure | Sci-Fi</span>
+        <div className="flex items-center gap-1">
+          <CalendarIcon className="w-4.5 h-4.5" /> 2018
+        </div>
+        <div className="flex items-center gap-1">
+          <ClockIcon className="w-4.5 h-4.5" /> 2h 8m
+        </div>
+      </div>
+      <p className="max-w-md text-gray-300">
+        In a post-apocalyptic world where cities ride on wheels and consume each
+        other to survive, two people in London and try to stop a conspiracy.
+      </p>
+      <button
+        className="flex items-center gap-1 px-6 py-3 text-sm bg-primary hover:bg-primary-dull transition rounded-full font-medium cursor-pointer"
+        onClick={() => navigate("/movies")}
+      >
+        Explore Movies <ArrowRight className="w-5 h-5" />
+      </button>
+    </section>
+  );
+};
+
+export default HeroSection;
