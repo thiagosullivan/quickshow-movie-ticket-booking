04268298f07e4a64f77124884c76fcac01890f48
thiagosullivan <thiagosullivan12@gmail.com>
2025-12-20 07:42:17 -0300

    feat: trailers section

---
+++ client/src/components/TrailersSection.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import ReactPlayer from "react-player";
+import { dummyTrailers } from "../assets/assets";
+import BlurCircle from "./BlurCircle";
+import { PlayCircleIcon } from "lucide-react";
+
+const TrailersSection = () => {
+  const [currentTrailer, setCurrentTrailer] = useState(dummyTrailers[0]);
+
+  return (
+    <div className="px-6 md:px-16 lg:px-24 xl:px-44 py-20 overflow-hidden">
+      <p className="text-gray-300 font-medium text-lg max-w-[960px] mx-auto">
+        Trailers
+      </p>
+      <div className="relative mt-6">
+        <BlurCircle top="-100px" right="-100px" />
+        <ReactPlayer
+          src={currentTrailer.videoUrl}
+          controls={false}
+          className="mx-auto max-w-full"
+          width="960px"
+          height="540px"
+        />
+      </div>
+
+      <div className="group grid grid-cols-4 gap-4 md:gap-8 mt-8 max-w-3xl mx-auto">
+        {dummyTrailers.map((trailer) => (
+          <div
+            className="relative group-hover:not-hover:opacity-50 hover:-translate-y-1 duration-300 transition max-md:h-60 md:max-h-60 cursor-pointer"
+            key={trailer.image}
+            onClick={() => setCurrentTrailer(trailer)}
+          >
+            <img
+              src={trailer.image}
+              alt="Trailer"
+              className="rounded-lg w-full h-full object-cover brightness-75"
+            />
+
+            <PlayCircleIcon
+              strokeWidth={1.6}
+              className="absolute top-1/2 left-1/2 w-5 md:w-8 md:h-12 transform -translate-x-1/2 -translate-y-1/2"
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default TrailersSection;
