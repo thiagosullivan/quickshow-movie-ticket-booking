0634be139ed1f5fe9111c8fcf22ca517d2d0986f
thiagosullivan <thiagosullivan12@gmail.com>
2025-12-28 11:07:37 -0300

    fix: correction names on create user on inngest

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -9,10 +9,11 @@ const syncUserCreation = inngest.createFunction(
   { id: "sync-user-from-clerk" },
   { event: "clerk/user.created" },
   async ({ event }) => {
-    const { id, first_name, last_name, email_address, image_url } = event.data;
+    const { id, first_name, last_name, email_addresses, image_url } =
+      event.data;
     const userData = {
       _id: id,
-      email: email_address[0].email_address,
+      email: email_addresses[0].email_address,
       name: first_name + " " + last_name,
       image: image_url,
     };
@@ -35,14 +36,14 @@ const syncUserUpdation = inngest.createFunction(
   { id: "update-user-from-clerk" },
   { event: "clerk/user.updated" },
   async ({ event }) => {
-    const { id, first_name, last_name, email_address, image_url } = event.data;
+    const { id, first_name, last_name, email_addresses, image_url } =
+      event.data;
     const userData = {
       _id: id,
-      email: email_address[0].email_address,
+      email: email_addresses[0].email_address,
       name: first_name + " " + last_name,
       image: image_url,
     };
-
     await User.findByIdAndUpdate(id, userData);
   }
 );
