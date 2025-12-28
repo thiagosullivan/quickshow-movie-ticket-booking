bb19fa9489d83d0f35ca8df42ab8cd48770eb0e5
thiagosullivan <thiagosullivan12@gmail.com>
2025-12-28 10:17:46 -0300

    feat: add inngest to webhook clerk

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
+import { clerkMiddleware } from "@clerk/express";
+import { serve } from "inngest/express";
+import { inngest, functions } from "./src/inngest";
 import connectDB from "./configs/db.js";
 
 const app = express();
@@ -11,7 +14,10 @@ await connectDB();
 // Middleware
 app.use(express.json());
 app.use(cors());
+app.use(clerkMiddleware());
 
+// API routes
 app.get("/", (req, res) => res.send("Server is Live!"));
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
