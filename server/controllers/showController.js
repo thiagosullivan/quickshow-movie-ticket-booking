dbf9a50f268dc51e8bd0d9da53c9df4cdeb3d231
thiagosullivan <thiagosullivan12@gmail.com>
2026-01-06 17:24:00 -0300

    (backend) feat: add endpoint to add show

--- server/controllers/showController.js
+++ server/controllers/showController.js
@@ -1,4 +1,8 @@
 import axios from "axios";
+import Movie from "../models/Movie.js";
+import Show from "../models/Show.js";
+
+// API to get now playing movies from TDBM
 
 export const getNowPlayingMovies = async (req, res) => {
   try {
@@ -14,6 +18,72 @@ export const getNowPlayingMovies = async (req, res) => {
     const movies = data.results;
 
     res.json({ sucess: true, movies });
+  } catch (error) {
+    console.error(error);
+    res.json({ sucess: false, message: error.message });
+  }
+};
+
+// API to add a new show to the database
+export const addShow = async (req, res) => {
+  try {
+    const { movieId, showsInput, showPrice } = req.body;
+
+    let movie = await Movie.findById(movieId);
+
+    if (!movie) {
+      // Fetch movie details and credits from TMDB API
+      const [movieDetailsResponse, movieCreditsResponse] = await Promise.all([
+        axios.get(`https://api.themoviedb.org/3/movie/${movieId}`, {
+          headers: { Authorization: `Bearer ${process.env.TMDB_API_KEY}` },
+        }),
+
+        axios.get(`https://api.themoviedb.org/3/movie/${movieId}/credits`, {
+          headers: { Authorization: `Bearer ${process.env.TMDB_API_KEY}` },
+        }),
+      ]);
+
+      const movieApiData = movieDetailsResponse.data;
+      const movieCreditsData = movieCreditsResponse.data;
+
+      const movieDetails = {
+        _id: movieId,
+        title: movieApiData.title,
+        overview: movieApiData.overview,
+        poster_path: movieApiData.poster_path,
+        backdrop_path: movieApiData.backdrop_path,
+        genres: movieApiData.genres,
+        casts: movieCreditsData.cast,
+        release_date: movieApiData.release_date,
+        original_language: movieApiData.original_language,
+        tagline: movieApiData.tagline || "",
+        vote_average: movieApiData.vote_average,
+        runtime: movieApiData.runtime,
+      };
+
+      //   Add movie to the database
+      movie = await Movie.create(movieDetails);
+    }
+
+    const showsToCreate = [];
+    showsInput.forEach((show) => {
+      const showDate = show.date;
+      show.time.forEach((time) => {
+        const dateTimeString = `${showDate}T${time}`;
+        showsToCreate.push({
+          movie: movieId,
+          showDateTime: new Date(dateTimeString),
+          showPrice,
+          occupiedSeats: {}, // Initialize with empty object
+        });
+      });
+    });
+
+    if (showsToCreate.length > 0) {
+      await Show.insertMany(showsToCreate);
+    }
+
+    res.json({ sucess: true, message: "Show Added Successfully!" });
   } catch (error) {
     console.error(error);
     res.json({ sucess: false, message: error.message });
