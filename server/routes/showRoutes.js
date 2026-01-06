069868192186b1ad37d7ce68ea39462dc974227d
thiagosullivan <thiagosullivan12@gmail.com>
2026-01-06 17:24:46 -0300

    (backend) feat: add middleware to protect show router

--- server/routes/showRoutes.js
+++ server/routes/showRoutes.js
@@ -3,7 +3,7 @@ import { addShow, getNowPlayingMovies } from "../controllers/showController.js";
 
 const showRouter = express.Router();
 
-showRouter.get("/now-playing", getNowPlayingMovies);
-showRouter.post("/add", addShow);
+showRouter.get("/now-playing", protectAdmin, getNowPlayingMovies);
+showRouter.post("/add", protectAdmin, addShow);
 
 export default showRouter;
