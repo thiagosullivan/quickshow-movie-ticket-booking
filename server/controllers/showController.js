85412684aef1eb937f66156bb88c4b049d177a64
thiagosullivan <thiagosullivan12@gmail.com>
2026-01-07 13:24:32 -0300

    (backend) feat: get single show

--- server/controllers/showController.js
+++ server/controllers/showController.js
@@ -106,3 +106,32 @@ export const getShows = async (req, res) => {
     res.json({ success: false, message: error.message });
   }
 };
+
+// API to get a single show from the database
+export const getShow = async (req, res) => {
+  try {
+    const { movieId } = req.params;
+    // get all upcoming shows for the movie
+    const shows = await Show.find({
+      movie: movieId,
+      showDateTime: { $gte: new Date() },
+    });
+
+    const movie = await Movie.findById(movieId);
+    const dateTime = {};
+
+    shows.forEach((show) => {
+      const date = show.showDateTime.toISOString().split("T")[0];
+      if (!dateTime[date]) {
+        dateTime[date] = [];
+      }
+
+      dateTime[date].push({ time: show.showDateTime, showId: show._id });
+    });
+
+    res.json({ success: true, movie, dateTime });
+  } catch (error) {
+    console.error(error);
+    res.json({ success: false, message: error.message });
+  }
+};
--- server/routes/showRoutes.js
+++ server/routes/showRoutes.js
@@ -12,5 +12,6 @@ const showRouter = express.Router();
 showRouter.get("/now-playing", protectAdmin, getNowPlayingMovies);
 showRouter.post("/add", protectAdmin, addShow);
 showRouter.get("/all", getShows);
+showRouter.get("/:movieId", getShow);
 
 export default showRouter;
