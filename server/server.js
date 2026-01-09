2eced173c50dd6b5114661bf1d63640ca222c579
thiagosullivan <thiagosullivan12@gmail.com>
2026-01-09 16:05:53 -0300

    (backend) feat: add admin endpoint

--- server/server.js
+++ server/server.js
@@ -7,6 +7,8 @@ import { inngest, functions } from "./inngest/index.js";
 import connectDB from "./configs/db.js";
 import showRouter from "./routes/showRoutes.js";
 import { bookingRouter } from "./routes/bookingRoutes.js";
+import adminRouter from "./routes/adminRoutes.js";
+import userRouter from "./routes/userRoutes.js";
 
 const app = express();
 const port = 3000;
@@ -23,5 +25,6 @@ app.get("/", (req, res) => res.send("Server is Live!"));
 app.use("/api/inngest", serve({ client: inngest, functions }));
 app.use("/api/show", showRouter);
 app.use("/api/booking", bookingRouter);
+app.use("/api/admin", adminRouter);
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
