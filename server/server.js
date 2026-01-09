4d776b8a71e4800bc297a8055cc48458b70ca5b9
thiagosullivan <thiagosullivan12@gmail.com>
2026-01-09 16:06:25 -0300

    (backend) feat: add user endpoint

--- server/server.js
+++ server/server.js
@@ -26,5 +26,6 @@ app.use("/api/inngest", serve({ client: inngest, functions }));
 app.use("/api/show", showRouter);
 app.use("/api/booking", bookingRouter);
 app.use("/api/admin", adminRouter);
+app.use("/api/user", userRouter);
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
