882c1228e5c3f45cad682ed2a503c1c692b2b4cd
thiagosullivan <thiagosullivan12@gmail.com>
2026-01-16 14:49:49 -0300

    (frontend) feat: fetch list movies  from db to Featured component

--- client/src/components/FeaturedSection.jsx
+++ client/src/components/FeaturedSection.jsx
@@ -1,12 +1,13 @@
 import { ArrowRight } from "lucide-react";
-import React from "react";
 import { useNavigate } from "react-router-dom";
 import BlurCircle from "./BlurCircle";
 import MovieCard from "./MovieCard";
-import { dummyShowsData } from "../assets/assets";
+import { useAppContext } from "../context/AppContext";
 
 const FeaturedSection = () => {
   const navigate = useNavigate();
+  const { shows } = useAppContext();
+
   return (
     <section className="px-6 md:px-16 lg:px-24 xl:px-44 overflow-hidden">
       <div className="relative flex items-center justify-between pt-20 pb-10">
@@ -21,8 +22,8 @@ const FeaturedSection = () => {
         </button>
       </div>
 
-      <div className="flex flex-wrap justify-center gap-8 mt-8">
-        {dummyShowsData.slice(0, 4).map((show) => (
+      <div className="flex flex-wrap justify-start gap-8 mt-8">
+        {shows.slice(0, 4).map((show) => (
           <MovieCard key={show._id} movie={show} />
         ))}
       </div>
