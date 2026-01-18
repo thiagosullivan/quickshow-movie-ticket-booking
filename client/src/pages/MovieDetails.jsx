0a079580d74675083f8527c10c8c8302dc2e0843
thiagosullivan <thiagosullivan12@gmail.com>
2026-01-17 21:47:56 -0300

    (frontend) feat: get movies details from db and add function to favorite movies

--- client/src/pages/MovieDetails.jsx
+++ client/src/pages/MovieDetails.jsx
@@ -1,27 +1,65 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
-import { dummyDateTimeData, dummyShowsData } from "../assets/assets";
 import BlurCircle from "../components/BlurCircle";
 import { Heart, PlayCircleIcon, StarIcon } from "lucide-react";
 import timeFormat from "../lib/timeFormat";
 import DateSelect from "../components/DateSelect";
 import MovieCard from "../components/MovieCard";
 import Loading from "../components/Loading";
+import toast from "react-hot-toast";
+import { useAppContext } from "../context/AppContext";
 
 const MovieDetails = () => {
   const navigate = useNavigate();
   const { id } = useParams();
-
   const [show, setShow] = useState(null);
+  const [isLoading, setIsLading] = useState(false);
+
+  const {
+    axios,
+    user,
+    getToken,
+    shows,
+    favoriteMovies,
+    fetchFavoriteMovies,
+    image_base_url,
+  } = useAppContext();
 
   const getShow = async () => {
-    const show = dummyShowsData.find((show) => show._id === id);
-    if (show) {
-      setShow({
-        movie: show,
-        dateTime: dummyDateTimeData,
-      });
+    try {
+      const { data } = await axios.get(`/api/show/${id}`);
+      if (data.success) {
+        setShow(data);
+      }
+    } catch (error) {
+      console.log(error);
     }
+  };
+
+  const handleFavorite = async () => {
+    setIsLading(true);
+    try {
+      if (!user) {
+        return toast.error("Please Login to proceed");
+      }
+      const { data } = await axios.post(
+        "/api/user/update-favorite",
+        {
+          movieId: id,
+        },
+        {
+          headers: { Authorization: `Bearer ${await getToken()}` },
+        },
+      );
+
+      if (data.success) {
+        await fetchFavoriteMovies();
+        toast.success(data.message);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+    setIsLading(false);
   };
 
   useEffect(() => {
@@ -32,7 +70,7 @@ const MovieDetails = () => {
     <main className="px-6 md:px-16 lg:px-40 pt-30 md:pt-50">
       <section className="flex flex-col md:flex-row gap-8 max-w-6xl mx-auto">
         <img
-          src={show.movie.poster_path}
+          src={image_base_url + show.movie.poster_path}
           alt={show.movie.title}
           className="max-md:mx-auto rounded-xl h-104 max-w-70 object-cover"
         />
@@ -67,8 +105,18 @@ const MovieDetails = () => {
             >
               Buy Tickets
             </a>
-            <button className="bg-gray-700 p-2.5 rounded-full transition cursor-pointer active:scale-95">
-              <Heart className={`w-5 h-5`} />
+            <button
+              onClick={handleFavorite}
+              disabled={isLoading}
+              className="bg-gray-700 p-2.5 rounded-full transition cursor-pointer active:scale-95"
+            >
+              {isLoading ? (
+                <div class="w-5 h-5 border-2 border-primary border-t-transparent rounded-full animate-spin"></div>
+              ) : (
+                <Heart
+                  className={`w-5 h-5 ${favoriteMovies.find((movie) => movie._id === id) ? "fill-primary text-primary" : ""}`}
+                />
+              )}
             </button>
           </div>
         </div>
@@ -80,7 +128,7 @@ const MovieDetails = () => {
           {show.movie.casts.slice(0, 12).map((cast, index) => (
             <div key={index} className="flex flex-col items-center text-center">
               <img
-                src={cast.profile_path}
+                src={image_base_url + cast.profile_path}
                 alt={cast.name}
                 className="rounded-full h-20 md:h-20 aspect-square object-cover"
               />
@@ -94,14 +142,14 @@ const MovieDetails = () => {
 
       <p className="text-lg font-medium mt-20 mb-8">You May Also Like</p>
       <div className="flex flex-wrap max-sm:justify-center gap-8">
-        {dummyShowsData.slice(0, 4).map((movie, index) => (
+        {shows.slice(0, 4).map((movie, index) => (
           <MovieCard key={index} movie={movie} />
         ))}
       </div>
       <div className="flex justify-center mt-20">
         <button
           onClick={() => {
-            navigate("/movies"), scrollTo(0, 0);
+            (navigate("/movies"), scrollTo(0, 0));
           }}
           className="px-10 py-3 text-sm bg-primary hover:bg-primary-dull transition rounded-md font-medium cursor-pointer"
         >
